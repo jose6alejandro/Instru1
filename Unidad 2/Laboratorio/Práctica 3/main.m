47f3744e571520d81076657b15165da7eca4bc18
jose6alejandro <joseacastrorosales@gmail.com>
2024-03-09 23:11:44 -0400

    modificado el cálculo del error

--- Unidad 2/Laboratorio/Práctica 3/main.m
+++ Unidad 2/Laboratorio/Práctica 3/main.m
@@ -1,8 +1,8 @@
-%Pr√°ctica 3 - Castro Jos√©, Hurtado Carlos.
+%Pr·ctica 3 - Castro JosÈ, Hurtado Carlos.
 clear all
 close all
 global Req R2 R3 Rm V C FREQ PERIODO;
-%Par√°metros del sistema
+%Par·metros del sistema
 
 FREQ = 378.8; 
 PERIODO = 1/FREQ;
@@ -24,13 +24,11 @@ tRango=[];
 tTotal=[0];
 
 condIni = [vcIni icIni];
-%Vector para acumular los valores de x en el ODE
 infODE = condIni;
 
-%Variables con las condiciones iniciales de MAPLE
 vcMaple = 0;
 icMaple = 0;
-%Vectores para acumular las C.I. de MAPLE
+
 VcODE = [vcIni];
 IcODE = [icIni];
 
@@ -76,13 +74,13 @@ IcODE = [icIni];
     tInicial = tTotal(end);
  end
 
-%Extracci√≥n de la data
+%ExtracciÛn de la data
 dvoltaje = importdata('Data/Voltaje.CSV',',',18);
 VoltajeData =  dvoltaje.data(:, 4:5);
 dcorriente = importdata('Data/Corriente.CSV',',',18);
 CorrienteData = dcorriente.data(:, 4:5);
 
-%Gr√°ficas Vc e Ic
+%Gr·ficas Vc e Ic
 figure(1);
 plot((tTotal),infODE(:,1),'g',(tTotal),VcODE,'r', (VoltajeData(:,1))+2.8594e-3, VoltajeData(:,2)-2.32,'b');
 title('Voltaje del capacitor');
@@ -99,10 +97,23 @@ ylabel('Voltaje (Vols)');
 legend('ODE', 'Maple', 'Experimental');
 grid on;
 
-%C√°lculo del error 
-longitud = length(tTotal);
-%errorVmae = mean(abs((VoltajeData(1:longitud,2) - VcODE)/ longitud))
-%errorImae = mean(abs(((CorrienteData(1:longitud,1)/Rm) - IcODE) / longitud))
-errorVmape = mean(abs((VoltajeData(1:longitud,2) - VcODE)./ VoltajeData(1:longitud,2)))/ longitud
-errorImape = mean(abs(((CorrienteData(1:longitud,2)/Rm) - IcODE)./(CorrienteData(1:longitud,1)/Rm)))/ longitud
+%C·lculo del error 
+
+tiempo  =(VoltajeData(:,1))+2.8594e-3;
+voltaje = VoltajeData(:,2);
+vcData  = [tiempo voltaje];
+condIni = -1.2;
+vcMaple = (((R2+R3+Req)*condIni-R2*V)*exp(-(R2+R3+Req)*(vcData(44:707,1))/(((R3+Req+Rm)*R2+Rm*(R3+Req))*C))+R2*V)/(R2+R3+Req) + 2.32;
+%plot(vcData(44:707,1),vcMaple,vcData(44:707,1),vcData(44:707,2));
+longitud = length(vcData(44:707,1));
+errorVmape = mean(abs((vcData(44:707,2)- vcMaple) ./ vcData(44:707,2))) * 100 / longitud
+
+
+condIni = 0.001438;
+corriente = CorrienteData(:,2)/Rm + 0.001;
+icData = [tiempo corriente];
+icMaple = exp(-(R2+R3+Req)*(vcData(44:700,1))/((R2*R3+R2*Req+R2*Rm+R3*Rm+Req*Rm)*C))*condIni + 0.001;
+%plot(icData(44:700,1),icMaple,icData(44:700,1),icData(44:700,2))
+longitud = length(icData(44:700,1));
+errorImape = mean(abs((icData(44:700,2)- icMaple) ./ icData(44:700,2))) * 100 / longitud
 
